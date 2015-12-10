0a610c308f6d2ee57be4b7ef13743918266f4439
addictedtocoding <agupta10000@yahoo.com>
2015-12-10 01:33:15 -0600

    Create Default.aspx.cs

---
+++ Default.aspx.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace Project
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            if(FormsAuthentication.Authenticate(TextBox1.Text,TextBox2.Text))
+            {
+                FormsAuthentication.RedirectFromLoginPage(TextBox1.Text, true);
+                Response.Redirect("upload.aspx");
+            }
+        }
+    }
+}
