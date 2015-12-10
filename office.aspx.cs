9b850b5ec13acc7c033462aa363906ec6a586fde
addictedtocoding <agupta10000@yahoo.com>
2015-12-10 12:11:07 -0600

    Create office.aspx.cs

---
+++ office.aspx.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace Project
+{
+    public partial class office : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void Ok_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
